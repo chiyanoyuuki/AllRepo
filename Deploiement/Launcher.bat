95f9bd169d8d1e98e7751f5178dbf0d673b2bf3f
chiyanoyuuki <charles.poure@hotmail.com>
2019-04-07 13:45:03 +0200

    07/04/2019

---
+++ Deploiement/Launcher.bat
@@ -0,0 +1,2 @@
+java -jar "jars/RecupIndices.jar"
+pause
---
+++ Java/RecupIndices/src/Recup/Indice.java
@@ -0,0 +1,39 @@
+package Recup;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Indice 
+{
+	private String nom;
+	private double last;
+	
+	public Indice(double val, String nom)
+	{
+		this.nom = nom;
+		this.last = val;
+	}
+	
+	public void addVal(double d, Statement st)
+	{
+		if(last!=d)
+		{
+			last = d;
+			try {
+				st.executeQuery("INSERT INTO VALEURS_INDICES (ID,VAL) VALUES ((SELECT ID FROM INDICES WHERE NOM='"+nom+"'),"+d+")");
+			} catch (SQLException e) 
+			{
+				show();
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void show()
+	{
+		System.out.print(String.format("%-60s", nom));
+		System.out.print(String.format("%-20s", "LAST : "+last));
+		System.out.println();
+	}
+}
